27a46f5977baaef3a2ac6f3981e5b54010162960
Mohammad Arfizur Rahman <arfiz0@gmail.com>
2020-05-08 23:04:06 +0600

    Completed streams project without server setup

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+	return [
+		{ title: "No Scrubs", duration: "4:05" },
+		{ title: "Macareno", duration: "3:35" },
+		{ title: "All Star", duration: "5:05" },
+		{ title: "I want it that way", duration: "4:55" },
+	];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === "SONG_SELECTED") {
+		return action.payload;
+	}
+	return selectedSong;
+};
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
